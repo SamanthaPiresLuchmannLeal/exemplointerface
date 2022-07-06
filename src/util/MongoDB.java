751e5648ba5d1b8fcc308ecfd3076866161b91e4
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-07-06 10:48:32 -0300

    Exemplo de interface

---
+++ src/util/MongoDB.java
@@ -0,0 +1,11 @@
+package util;
+
+import modelo.Conta;
+
+public class MongoDB implements BancoDeDados{
+
+    @Override
+    public void gravar(Conta conta) {
+        System.out.printf("Gravando no BD MongoDB a conta: %d\n", conta.getNumero());
+    }
+}
